5a7625c52b2331abfff372d4cc08e907a9110406
Vicki <ymasakot@gmail.com>
2022-09-05 23:20:01 +0800

    init

---
+++ 2_ShoppingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2_ShoppingApp
+//
+//  Created by Vicki Yang on 2022/9/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
